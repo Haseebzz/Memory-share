dca1057013a7508e0dd5a2488cc9547a2da7b560
Haseebzz <chaudhuryhaseeb35@gmail.com>
2023-06-12 14:16:37 -0400

    finished connecting frontend to backend

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>About...
-        write some information about website style and include images
+        write some information about website style and include images!
     </div>
   )
 }
