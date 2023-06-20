afa2cbabe8f354204c17abfb4fd0b8bec9af1554
Talike <talikeb20@gmail.com>
2023-06-20 14:39:12 -0400

    styled navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 
 
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import CreateMemory from './pages/CreateMemory';
@@ -9,21 +9,21 @@ import UserMemory from './pages/UserMemory';
 import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import './App.css'
+import './css/App.css';
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar/>
-         <Routes>
-         <Route path='/' element={<Home/>} />
-         <Route path="/about" element={<About/>} />
-         <Route path="/create" element={<CreateMemory/>} />
-         <Route path="/login" element={<Login />} />
-         <Route path="/register" element={<Register/>} />
-         <Route path="/usermemory" element={<UserMemory/>} />
-         <Route path="/profile" element={<Profile/>} />
-         </Routes>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/create" element={<CreateMemory />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/usermemory" element={<UserMemory />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </Router>
     </div>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
+import "../css/Navbar.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -11,27 +12,44 @@ const Navbar = () => {
     window.localStorage.clear();
     navigate("/")
     window.location.reload();
-   
-}
+
+  }
+
+  useEffect(() => {
+    const navRight = document.getElementById("right-section");
+
+    if (!cookies.access_token) {
+      navRight.style.gridTemplateColumns = "1fr 1fr 1fr";
+      navRight.style.gridTemplateAreas = '"about register login"';
+    } else {
+      navRight.style.gridTemplateColumns = "1fr 1fr 1fr 1fr 1fr";
+      navRight.style.gridTemplateAreas = '"about profile create memories logout"';
+    }
+
+  });
 
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      {!cookies.access_token && (
-        <>
-          <Link to="/register">Register</Link>
-          <Link to="/login">Login</Link>
-        </>
-      )}
-      {cookies.access_token && (
-        <>
-          <Link to="/profile">Profile</Link>
-          <Link to="/create">Create</Link>
-          <Link to="/usermemory">MyMemory</Link>
-          <button onClick={logout}>Logout</button>
-        </>
-      )}
+    <div className="navbar">
+      <div id="left-section">
+        <Link to="/" id="home" className="goh">Memory Share</Link>
+      </div>
+      <div id='right-section'>
+        <Link to="/about" id="about" className="goh">About</Link>
+        {!cookies.access_token && (
+          <>
+            <Link to="/register" id="register" className="goh">Register</Link>
+            <Link to="/login" id="login" className="goh">Login</Link>
+          </>
+        )}
+        {cookies.access_token && (
+          <>
+            <Link to="/profile" id="profile" className="goh">Profile</Link>
+            <Link to="/create" id="create" className="goh">Create</Link>
+            <Link to="/usermemory" id="memories" className="goh">MyMemory</Link>
+            <button onClick={logout} id="logout" className="goh">Logout</button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
