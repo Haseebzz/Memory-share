0c4e5d81d8ece57e66d4cb4d522ece4c4a10667e
Muhammad Rahman <mrahman041@citymail.cuny.edu>
2023-07-23 19:49:16 -0400

    Login Page was improved styllistically

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,66 +1,58 @@
-import React, {useState} from 'react'
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { useCookies} from "react-cookie"
+import { useCookies } from 'react-cookie';
+import '../css/Login.css';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const[_, setCookies] = useCookies(["access_token"]);
-    const navigate = useNavigate();
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try{
-          const result = await axios.post('http://localhost:4000/auth/login', {
-              username,
-              password
-          })
-          setCookies("access_token", result.data.token);
-          window.localStorage.setItem("userID", result.data.userID);
-          window.localStorage.setItem("username", result.data.username);
-          navigate("/");
-      } catch (error) {
-          console.error(error);
-      }
-      };
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [_, setCookies] = useCookies(['access_token']);
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post('http://localhost:4000/auth/login', {
+        username,
+        password,
+      });
+      setCookies('access_token', result.data.token);
+      window.localStorage.setItem('userID', result.data.userID);
+      window.localStorage.setItem('username', result.data.username);
+      navigate('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>
-        <form  onSubmit={handleSubmit}>
-  
-      <label  htmlFor="username">
-        Username:
-      </label>
-      <input
-        id="username"
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Enter your username"
-      />
-  
-   
-      <label htmlFor="password">
-        Password:
-      </label>
-      <input
-       
-        id="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Enter your password"
-      />
+    <div className="login-container">
+      <div className="login-box">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter your username"
+          />
 
-   
-      <button
-        type="submit"
-      >
-        Login
-      </button>
- 
-  </form>
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+          />
+
+          <button type="submit">Login</button>
+        </form>
+        <div className="forgot-password">Forgot password?</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
