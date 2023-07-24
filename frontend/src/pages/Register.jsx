14210d9bdf6bc03f90b8f1b32bc85b7e79087bac
MystProgrammer <102705914+MystProgrammer@users.noreply.github.com>
2023-07-23 20:27:48 -0400

    Finish Register page with styling.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,58 +1,56 @@
-import React, {useState} from 'react'
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-const Register = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try{
-          await axios.post('http://localhost:4000/auth/register', {
-              username,
-              password,
-          })
-          alert("Registeration Completed! Now Login");
-          navigate("/login")
-        } catch (error) {
-          console.log(error);
-        }
-      };
+import '../css/Register.css';
 
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:4000/auth/register', {
+        username,
+        password,
+      });
+      alert('Registeration Completed! Now Login');
+      navigate('/login');
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div>
-     <form  onSubmit={handleSubmit}>
-      <label htmlFor="username">
-        Username:
-      </label>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Enter your username"
-      />
-      <label htmlFor="password">
-        Password:
-      </label>
-      <input
-        id="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="**********"
-      />
-   
-   
-      <button
-        type="submit"
-      >
-        Register
-      </button>
-  </form>
-    </div>
-  )
-}
+    <div className="Body">
+      <div className="Box">
+        <form onSubmit={handleSubmit}>
+          <div className="TextBoxes">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter your username"
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="**********"
+            />
 
-export default Register
+            <button className="registerButton" type="submit">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
